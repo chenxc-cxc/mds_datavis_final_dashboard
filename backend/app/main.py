9d1197a1d98ace4a830b732c0826ff467b0f6feb
cswun <zpchen@czps-404.local>
2025-12-01 23:05:31 +0800

    add origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,9 @@ app = FastAPI(title=settings.project_name, openapi_url=f"{settings.api_prefix}/o
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://mds-datavis-final-dashboard-1.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
